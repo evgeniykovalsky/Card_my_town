92579fe0feb42d755a453a162812fa9072444ff7
Evgeniy <evgeniykovalsky04386@gmail.com>
2022-08-12 21:38:44 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import React, { useState } from 'react';
+
+
+
+function App(props) {
+
+ const[description,setDescription]= useState(false)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+       <div className="conteiner">
+    <div className="head">{props.data.town}</div>
+
+    <div className="mainBody">
+        <div className="picture"> 
+        <img src={props.data.image1} alt={props.data.id}/>
+        <img src={props.data.image2} alt={props.data.id}/>
+        <img src={props.data.image3} alt={props.data.id}/>
+        </div>
+        <div className="description">
+            <p><span>Город :</span> {props.data.town}</p>
+            <p><span>Страна:</span> {props.data.country}</p>
+            <p><span>Год основания:</span> {props.data.yearOfBorn}</p>
+            <p className="lsDesc">{props.data.description}</p>
+           
+        </div>
     </div>
+    <div className="footer">
+        <button 
+            onClick={()=>setDescription(prev =>!prev)}
+            >{description? 'Cкрыть':'Подробнее..'}</button>
+            {description && <p>{props.data.allDescription}</p>}
+    </div>
+    
+ </div>
+   
   );
 }
 
